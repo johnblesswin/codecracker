6bfdc32757a4db8af2e2a6816af916dafd78239b
Mukul Mantosh <mukulmantosh@yahoo.com>
2016-08-07 17:46:18 +0530

    initial liveapp testing 3

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -6,33 +6,25 @@ ini_set('session.cookie_httponly', '1');
 
 class DBConnection
 {
-    $app_username = getenv('app_username');
-    $app_password = getenv('app_password');
-    $app_database = getenv('app_database');
+   
 
     const HOST = 'localhost';
     const PORT = 27017;
-    const DBNAME = $app_database;
-    const USERNAME = $app_password;
-    const PASSWORD = $app_password;
     private static $instance;
     public $connection;
     public $database;
     private function __construct()
     {
         
-        $connectionString = sprintf('mongodb://%s:%s@ds145395.mlab.com:45395/%d', 
-            DBConnection::USERNAME,DBConnection::PASSWORD, 
-            DBConnection::DBNAME);
-
-        
         try {
-            $this->connection = new MongoClient($connectionString);
-            $this->database   = $this->connection->selectDB(DBConnection::DBNAME);
+            $this->connection = new MongoClient('mongodb://$app_username:$app_password@ds145395.mlab.com:45395/$app_database');
+
+            $this->database   = $this->connection->selectDB($app_database);
         }
         catch (MongoConnectionException $e) {
             die("Server not connected");
         }
+        
     }
     static public function instantiate()
     {
